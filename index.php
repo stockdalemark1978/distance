6a205c59882ac66562d5f092d4ffb46bcc5267d2
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-07-22 17:41:36 -0400

    started basic template, added background and form

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>How Far Is It?</title>
+
+   <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="jumbotron">
+        <div class="container-fluid">
+            <h1>How Far Is It?</h1>
+            <p>Enter Your Starting & Ending Destinations To Calculate The Mileage Between Them.</p>
+            <form action="" class="form-horizontal">
+                <div class="form-group">
+                    <label class="col-xs-2 control-label" for="start">Starting Location:</label>
+                    <div class="col-xs-10">
+                        <input class="form-control" type="text" id="start" placeholder="Starting Location">
+                    </div>
+                    <label class="col-xs-2 control-label" for="end">Ending Location:</label>
+                    <div class="col-xs-10">
+                        <input class="form-control" type="text" id="end" placeholder="Ending Location">
+                    </div>
+                </div>
+            </form>
+        </div>
+        
+    </div>
+  
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
