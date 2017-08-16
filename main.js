66ae9ae618495dde7c0594032a59d56ab13031f6
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-08-16 17:06:17 -0400

    adding javascript for map funtions

--- main.js
+++ main.js
@@ -10,3 +10,29 @@
     
     //create map
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    //create a DirectionsService object to get the results
+    var directionsService = new google.maps.DirectionsService();
+
+    //create DirectionsREnderer object which displays the route
+    var directionsDisplay = new google.maps.DirectionsRenderer();
+
+    //bind the DirectionsRenderer to the map
+        directionsDisplay.setMap(map);
+
+    //define calcRoute function
+    funtion calcRoute() {
+        //create request
+
+        //pass the request to the route method
+            //if status OK
+                //Get distance and time
+
+                //display route using DirectionsRenderer object
+
+                //if status not OK
+                    //delete route from map
+
+                    //center map in London
+
+    }
